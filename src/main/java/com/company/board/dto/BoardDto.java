c9198d970d08073ad969a55fb1887e2db83f761f
nicef <nicef@DESKTOP-PDFN3L3>
2023-06-16 12:08:17 +0900

    수정 사항

--- src/main/java/com/company/board/dto/BoardDto.java
+++ src/main/java/com/company/board/dto/BoardDto.java
@@ -3,16 +3,19 @@ package com.company.board.dto;
 import java.sql.Timestamp;
 
 public class BoardDto {
-	private int bid;
-	private String bname;
-	private String btitle;
-	private String bcontent;
-	private Timestamp bdate;
-	private int bhit;
+	
+	private int bid;//게시판 글번호
+	private String bname;//글쓴이 아이디
+	private String btitle;//글 제목
+	private String bcontent;//글 내용
+	private Timestamp bdate;//글 등록일시
+	private int bhit;//조회수
+	
 	public BoardDto() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public BoardDto(int bid, String bname, String btitle, String bcontent, Timestamp bdate, int bhit) {
 		super();
 		this.bid = bid;
@@ -22,42 +25,55 @@ public class BoardDto {
 		this.bdate = bdate;
 		this.bhit = bhit;
 	}
+
 	public int getBid() {
 		return bid;
 	}
+
 	public void setBid(int bid) {
 		this.bid = bid;
 	}
+
 	public String getBname() {
 		return bname;
 	}
+
 	public void setBname(String bname) {
 		this.bname = bname;
 	}
+
 	public String getBtitle() {
 		return btitle;
 	}
+
 	public void setBtitle(String btitle) {
 		this.btitle = btitle;
 	}
+
 	public String getBcontent() {
 		return bcontent;
 	}
+
 	public void setBcontent(String bcontent) {
 		this.bcontent = bcontent;
 	}
+
 	public Timestamp getBdate() {
 		return bdate;
 	}
+
 	public void setBdate(Timestamp bdate) {
 		this.bdate = bdate;
 	}
+
 	public int getBhit() {
 		return bhit;
 	}
+
 	public void setBhit(int bhit) {
 		this.bhit = bhit;
 	}
 	
 	
+
 }
